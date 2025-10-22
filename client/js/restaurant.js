fe65b03b7d10949ea4c4de2bb9e43b82930bf19e
radiahteur <radiah.mohamed.assoweh@umontreal.ca>
2025-10-22 15:49:42 -0400

    Initial commit - site

---
+++ client/js/restaurant.js
@@ -0,0 +1,29 @@
+const menu = {
+    Starters :   [
+        {nom:"Salade César", prix:5.50, image:"Salade César"},
+        { nom: "Salade Composée", prix: 5.50, image: "Salade Composée.jpg" },
+        { nom: "Salade Niçoise", prix: 5.50, image: "Salade Niçoise.jpg" },
+        { nom: "Saumon fumé", prix: 6.50, image: "Saumon fumé.jpg" },
+        { nom: "Potage", prix: 4.00, image: "Potage.jpg" }
+    ],
+    Maindishes :  [
+        {nom:"Tomato sauce penne", prix:16.40, image:"Penne sauce tomate.jpg"},
+        { nom: "Chicken fusilli", prix: 13.50, image: "Fusilli au poulet.jpg" },
+        { nom: "Paella", prix: 15.00, image: "Paella.jpg" },
+        { nom: "Cantonese rice", prix: 17.20, image: "Riz cantonnais.jpg" },
+        { nom: "Roasted chicken with fries", prix: 14.20, image: "Poulet-frites.jpg" },
+        { nom: "Shrimp pasta", prix: 14.20, image: "Spaghetti aux crevettes.jpg" }
+    ],
+    Desserts :  [
+        {nom:"Dame glacée", prix:6.30, image:"Dame glacée.jpg"},
+        { nom: "Chocolate cheesecake", prix: 8.50, image: "cheesecake chocolat.jpg" },
+        { nom: "Chocolate fondant", prix: 7.50, image: "fondant au chocolat.jpg" },
+        { nom: "Tiramisu", prix: 7.50, image: "tiramisu.jpg" }
+    ],
+    Drinks :  [
+        {nom:"Fresh juices", prix:6.75, image:"jus de fruits.jpg"},
+        { nom: "Mint lemonade", prix: 5.50, image: "Limonade menthe.jpg" },
+        { nom: "Milkshakes", prix: 7.50, image: "milkshake.jpg" },
+        { nom: "Ice-tea", prix: 6.50, image: "thé glacé.jpg" }
+    ]
+}
